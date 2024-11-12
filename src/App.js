28126c5e2dab37f4348090a02c864f80a847b938
HP <ularezephaniah@gmail.com>
2024-11-12 08:04:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Snacks from './pages/Snacks';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Help from "./pages/Help";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About/>} />
+        
+          <Route path="help" element={<Help/>} />  
+          <Route path="snacks" element={<Snacks/>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+export default function About(){
+  return(
+    <div className="About">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates perspiciatis excepturi tempore nemo dignissimos accusantium sapiente laudantium modi delectus nisi voluptate, sit quia voluptas minus quaerat facilis itaque tempora doloremque!</p>
+    </div>
+  )
+}
